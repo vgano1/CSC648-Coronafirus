16e608dadacafd6da14cf5ac1a9e39ff0857046c
Souhib Trabelsi <souhib.trabelsi@epitech.eu>
2020-10-12 23:59:17 +0200

    add search of wildfire by name

--- application/backend/api/routes.py
+++ application/backend/api/routes.py
@@ -1,9 +1,9 @@
 from api.coronavirus.resource import Covid, CovidByCountie
-from api.wildfire.resource import Fires, FireByCountie
+from api.wildfire.resource import FireList, FireByCountie, FireByName
 
 def initialize_routes(api):
     api.add_resource(Covid, '/coronavirus/counties')
     api.add_resource(CovidByCountie, '/coronavirus/countie/<string:countie>')
-    api.add_resource(Fires, '/wildfire/counties')
+    api.add_resource(FireList, '/wildfire/counties')
+    api.add_resource(FireByName, '/wildfire/firename/<string:name>')
     api.add_resource(FireByCountie, '/wildfire/countie/<string:countie>')
-
--- application/backend/api/wildfire/resource.py
+++ application/backend/api/wildfire/resource.py
@@ -1,8 +1,8 @@
-from flask import Response, request, jsonify, url_for, Response
+from flask import Response
 from flask_restful import Resource
 import pandas as pd
 
-class Fires(Resource):
+class FireList(Resource):
     def get(self):
         df = pd.read_csv(r'E:\Travail\Projets\csc648-02-fa20-team01\application\backend\api\static\mapdataall.csv')
         return Response(df.to_json(orient="records"), mimetype='application/json')
@@ -11,6 +11,15 @@ class FireByCountie(Resource):
     def get(self, countie):
         df = pd.read_csv(r'E:\Travail\Projets\csc648-02-fa20-team01\application\backend\api\static\mapdataall.csv')
         df = df[df['incident_county'] == countie]
+        if (df.empty) == False:
+            return Response(df.to_json(orient="records"), mimetype='application/json')
+        else:
+            return Response("Countie doesn't exist", 400)
+
+class FireByName(Resource):
+    def get(self, name):
+        df = pd.read_csv(r'E:\Travail\Projets\csc648-02-fa20-team01\application\backend\api\static\mapdataall.csv')
+        df = df[df['incident_name'] == name]
         if (df.empty) == False:
             return Response(df.to_json(orient="records"), mimetype='application/json')
         else:
