c0882685cc52106ce097c5b45210fde2f4fd5429
Amily Wu <amilyfwu@gmail.com>
2020-10-30 16:17:01 -0700

    authentication for login works! -Amily

--- application/frontend/amily-test/src/components/login.js
+++ application/frontend/amily-test/src/components/login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -30,8 +31,31 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+
 export default function SignIn() {
   const classes = useStyles();
+  const [email,setEmail] = React.useState('');
+  const [password,setPassword] = React.useState('');
+
+  const getPassword = (g) => {
+    setPassword(g.target.value);
+  };
+  const getEmail = (g) => {
+    setEmail(g.target.value);
+  }
+  const authenticate = (g) => {
+    const data = {
+      email: email,
+      password: password,
+    }
+    axios.post('http://ec2-15-237-111-31.eu-west-3.compute.amazonaws.com:5000/director-login/', data)
+      .then(res =>{
+        console.log(res.data);
+      })
+      .catch((e) => {
+        console.log(e);
+    });
+  };
 
   return (
     <Container component="main" maxWidth="xs">
@@ -43,7 +67,7 @@ export default function SignIn() {
         <Typography component="h1" variant="h5">
           Sign in
         </Typography>
-        <form className={classes.form} noValidate>
+        <div className={classes.form}>
           <TextField
             variant="outlined"
             margin="normal"
@@ -54,6 +78,7 @@ export default function SignIn() {
             name="email"
             autoComplete="email"
             autoFocus
+            onChange = {getEmail}
           />
           <TextField
             variant="outlined"
@@ -65,17 +90,18 @@ export default function SignIn() {
             type="password"
             id="password"
             autoComplete="current-password"
+            onChange = {getPassword}
           />
           <Button
-            type="submit"
             fullWidth
             variant="contained"
             color="primary"
             className={classes.submit}
+            onClick = {authenticate}
           >
             Sign In
           </Button>
-        </form>
+        </div>
       </div>
       <Box mt={8}>
       </Box>
--- application/frontend/amily-test/src/pages/privatepage.js
+++ application/frontend/amily-test/src/pages/privatepage.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import { Switch, Route, Link, Redirect} from 'react-router-dom';
+import FirePage from './pages/firepage';
+import CovidPage from './pages/covidpage';
 
+// {isLoggedIn && (
+//     <div>
+//         <Switch>
+//             <Route path = "/Fire" component ={FirePage}/>
+//             <Route path = "/Covid" component ={CovidPage}/>
+//         </Switch>
+//     </div>
+// )}
+// {!isLoggedIn && (
+//     <div>
+//         <Redirect to = "/3gfsd3t"/>
+//     </div>
+// )}
 
 const PrivatePage = () => {
     return (
         <div>
-            {isLoggedIn && (
-                <div>
 
-                </div>
-            )}
-            {!isLoggedIn && (
-                <div>
-                    <Redirect to = "/"/>
-                </div>
-            )}
         </div>
     );
 
