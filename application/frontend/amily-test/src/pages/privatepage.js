0bf74da683a964b2e352c32631575e3cc3637777
Amily Wu <amilyfwu@gmail.com>
2020-10-30 13:51:03 -0700

    trying to do private route that only people who login can access

---
+++ application/frontend/amily-test/src/pages/privatepage.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Switch, Route, Link, Redirect} from 'react-router-dom';
+
+
+const PrivatePage = () => {
+    return (
+        <div>
+            {isLoggedIn && (
+                <div>
+
+                </div>
+            )}
+            {!isLoggedIn && (
+                <div>
+                    <Redirect to = "/"/>
+                </div>
+            )}
+        </div>
+    );
+
+}
+
+export default PrivatePage;
