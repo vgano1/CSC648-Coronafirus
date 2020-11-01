4c55412759f5993913354dcc021eab46869fc6a0
Amily Wu <amilyfwu@gmail.com>
2020-11-01 14:43:57 -0800

    I replaced componentc class now

--- application/frontend/amily-test/src/components/countyDataEdit2.js
+++ application/frontend/amily-test/src/components/countyDataEdit2.js
@@ -1,13 +1,51 @@
 import React from 'react';
-import CountyDataEdit from '../components/countyDataEdit';
+import axios from 'axios';
+//import CountyDataEdit from '../components/countyDataEdit';
 
 const CountyDataEdit2 = () => {
     const [result, setResult] = React.useState({});
+    const [confirmed, setConfirmed] = React.useState(0);
+    const [death, setDeath]  = React.useState(0);
+    const [recovered, setRecovered]  = React.useState(0);
+    const [countie,setCountie] = React.useState('');
+
+    const updateConfirmed= (g)=>{
+        console.log(g.target.value);
+        setConfirmed(parseInt(g.target.value));
+    };
+    const updateDeath= (g)=>{
+        setDeath(parseInt(g.target.value));
+    };
+    const updateRecovered= (g)=>{
+        setRecovered(parseInt(g.target.value));
+    };
+    const updateCountie= (g)=>{
+        setCountie(g.target.value);
+    };
+
+    const submit1 = () =>{
+        const data = {
+            'confirmed': confirmed,
+            'countie': countie,
+            'death': death,
+            'recovered': recovered
+            
+        };
+        console.log(data);
+        axios.post('http://ec2-15-237-111-31.eu-west-3.compute.amazonaws.com:5000/update-covid/', data)
+            .then((res) => {
+                console.log(res);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
+
     React.useEffect(() => {
         //need account's county area and name
         let countyArea;
         let name;
-        fetch('http://ec2-15-237-111-31.eu-west-3.compute.amazonaws.com:5000/director-countie/?did=1')
+        fetch('http://ec2-15-237-111-31.eu-west-3.compute.amazonaws.com:5000/director-countie-covid/?did=1')
         .then(res => res.json())
         .then(resData => {
             console.log(resData[0]);
@@ -19,7 +57,6 @@ const CountyDataEdit2 = () => {
         });
 
     }, []); 
-
 
     return (
         <div>
@@ -39,7 +76,26 @@ const CountyDataEdit2 = () => {
                 <div>Case Fatality Ratio : {result['Case-Fatality_Ratio']}</div>
                 {/* {console.log(result)} */}
                 
-                <CountyDataEdit></CountyDataEdit>
+                <div>
+                    <div>change/update data and stat WIP</div>
+                    <div>
+                        
+                        <label for = "confirmed">Confirmed : </label>
+                        <input type ="number" name="confirmed" value={confirmed} onChange ={updateConfirmed}></input>
+    
+                        <label for = "death"> Deaths : </label>
+                        <input type ="number" name="death" value={death} onChange ={updateDeath}></input>
+    
+                        <label for = "recovered">Recovered : </label>
+                        <input type ="number" name="recovered" value={recovered} onChange ={updateRecovered}></input>
+                        
+                        <label for = "countie">County :</label>
+                        <input type ="text" name="countie" value={countie} onChange ={updateCountie}></input>
+    
+                        <div class="button-data-edit"></div>
+                            <button type="submit" onClick = {submit1}>Submit</button>
+                    </div>
+                </div>
             </div>
         </div>
     );
