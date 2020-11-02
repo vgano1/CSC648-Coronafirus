3f5623466a4e1a964cf09f2179023bcd7d9d8e17
Amily Wu <amilyfwu@gmail.com>
2020-11-01 16:01:45 -0800

    county/covid pages can now grab user information

--- application/frontend/amily-test/src/components/countyMain.js
+++ application/frontend/amily-test/src/components/countyMain.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const CountyMain = () => {
+    //county data and stats info
     const [result, setResult] = React.useState({});
+    //user info
+    const information = useSelector(state => state.userReducer.information);
+
+    //need account's county area and name
+    let countyArea = information.countie;
+    let name = information.name;
+
     React.useEffect(() => {
-        //need account's county area and name
-        let countyArea;
-        let name;
-        fetch('http://ec2-15-237-111-31.eu-west-3.compute.amazonaws.com:5000/coronavirus/countie/Napa')
+        fetch('http://ec2-15-237-111-31.eu-west-3.compute.amazonaws.com:5000/coronavirus/countie/' + countyArea)
         .then(res => res.json())
         .then(resData => {
             console.log(resData[0]);
@@ -23,8 +29,8 @@ const CountyMain = () => {
         <div>
             <div class="top-right">
                 <div>Show name and associate county/area</div>
-                <div>County name:</div>
-                <div>{result.Admin2}</div>
+                <div>Name : {name}</div>
+                <div>County : {countyArea}</div>
             </div>
 
             <div class = "left">
