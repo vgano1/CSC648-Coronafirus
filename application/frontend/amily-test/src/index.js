96537d40befd6b7de6520a524f5431efc105b388
Amily Wu <amilyfwu@gmail.com>
2020-11-02 17:41:36 -0800

    temporary remove browserrouter and provider in Index.js

--- application/frontend/amily-test/src/index.js
+++ application/frontend/amily-test/src/index.js
@@ -1,23 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {BrowserRouter} from 'react-router-dom';
+//import {BrowserRouter} from 'react-router-dom';
 
-import {createStore} from 'redux'; //make data store for the app
-import rootReducer from './redux/reducers/rootReducer';
+//import {createStore} from 'redux'; //make data store for the app
+//import rootReducer from './redux/reducers/rootReducer';
 //Allows components to access store via context
-import {Provider} from 'react-redux';
+//import {Provider} from 'react-redux';
 //create global datastore for the app
 
-const store = createStore(rootReducer);
+//const store = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider  store = {store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
