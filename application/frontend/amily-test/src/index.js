deea1c91d677a3a1b7dcc02affee6f5982a11b82
Amily Wu <amilyfwu@gmail.com>
2020-11-02 21:34:11 -0800

    restoring and can compile

--- application/frontend/amily-test/src/index.js
+++ application/frontend/amily-test/src/index.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-//import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 
-//import {createStore} from 'redux'; //make data store for the app
-//import rootReducer from './redux/reducers/rootReducer';
+import {createStore} from 'redux'; //make data store for the app
+import rootReducer from './redux/reducers/rootReducer';
 //Allows components to access store via context
-//import {Provider} from 'react-redux';
+import {Provider} from 'react-redux';
 //create global datastore for the app
 
-//const store = createStore(rootReducer);
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider  store = {store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
